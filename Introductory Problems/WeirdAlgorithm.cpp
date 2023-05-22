3ea73b2f05264a933975879779ea93c9b593175b
Ethan Havemann <ethave@amazon.com>
2023-05-22 11:46:22 -0500

    1/300

--- Introductory Problems/WeirdAlgorithm.cpp
+++ Introductory Problems/WeirdAlgorithm.cpp
@@ -5,16 +5,25 @@ using namespace std;
 
 void solve()
 {
-    // your code here
+	ll n;
+	cin >> n;
+	while(n!=1) {
+		cout << n << " ";
+		if (n%2 == 0) {
+			n = n/2;
+		}
+		else {
+			n = n*3 + 1;
+		}
+	}
+	cout << 1;
+	return;
 }
 
 int main()
 {
 	ios::sync_with_stdio(0);
 	cin.tie(0), cout.tie(0);
-	ll t;
-	cin >> t;
-	while (t--)
     	{
         	solve();
     	}
