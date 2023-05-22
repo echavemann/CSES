f568890887d86403e09158a469f269e3a3038a3e
Ethan Havemann <ethave@amazon.com>
2023-05-22 10:43:55 -0500

    wip 1

---
+++ Introductory Problems/WeirdAlgorithm.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <bits/stdc++.h>
+#define ll long long
+using namespace std;
+
+void solve()
+{
+    // your code here
+}
+
+int main()
+{
+	ios::sync_with_stdio(0);
+	cin.tie(0), cout.tie(0);
+	ll t;
+	cin >> t;
+	while (t--)
+    	{
+        	solve();
+    	}
+    	return 0;
+}
