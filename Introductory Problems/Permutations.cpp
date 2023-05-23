ae9956796346d3ed6aa89845d4198408a7b21141
Ethan Havemann <ethave@amazon.com>
2023-05-22 20:11:09 -0500

    1.6%

--- Introductory Problems/Permutations.cpp
+++ Introductory Problems/Permutations.cpp
@@ -6,16 +6,35 @@ using namespace std;
 void solve()
 {
 	int n;
-    cin >> n;
-    if (n < 5) cout << "NO SOLUTION" << endl;
-    int nums[n];
-    int j = 0;
-    if (n%2 == 1)  cout << n << endl;
-    for (int i = 0; i < n/2; i++) {
-        nums[2*i] = i+1; 
-        nums[(2*i) + 1] = n-(i-1);
-    }
-    for (int z : nums) cout << z << " ";
+    	cin >> n;
+    	if ((n == 3) || (n==2)) {
+		cout << "NO SOLUTION" << endl;
+		return;
+	}
+	else if (n==1) {
+		cout << 1 << endl;
+		return;
+	}
+	else if (n== 4) {
+		cout << 3 << " " << 1 << " " << 4 << " " << 2;
+		return;
+	}
+    	int nums[n];
+    	int j = 1;
+	int k = n;
+    	if (n%2 == 1)  cout << (n/2)+1 << " ";
+    	for (int i = 0; i < n/2; i++) {
+        	nums[2*i] = j;
+		j+=1;
+        	nums[(2*i) + 1] = k;
+		k--;
+    	}
+	if (n%2 == 0) cout << nums[n-1] << " ";
+	for (int i = 0; i < n-1; i++) {
+		cout << nums[i] << " "; 
+	}
+	if (n %2 ==1) cout << nums[n-1];
+
     
 
 }
